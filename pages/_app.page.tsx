08fe18df5c048bfbfec9afa46b198081908a40d3
Jacek Panasiuk <panasiuk.jc@gmail.com>
2023-02-01 11:44:55 +0100

    Move queryClient instance to a separate file (making it easier to find/import later)

--- pages/_app.page.tsx
+++ pages/_app.page.tsx
@@ -2,15 +2,8 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
 import { ChakraProvider } from '@chakra-ui/react'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      retry: false,
-    },
-  },
-})
+import { QueryClientProvider } from '@tanstack/react-query'
+import { queryClient } from './queryClient'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
