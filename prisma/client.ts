07696fe9455d4f18600ac278e11af7b4dfcb9f8a
Jacek Panasiuk <panasiuk.jc@gmail.com>
2023-02-11 10:06:01 +0100

    Add a workaround for prisma spawning too many clients in development.
    "warn(prisma-client) There are already 10 instances of Prisma Client actively running."
    
    (This seems to be a known issue with prisma + next)

--- prisma/client.ts
+++ prisma/client.ts
@@ -1,3 +1,23 @@
 import { PrismaClient } from '@prisma/client'
 
-export const prisma = new PrismaClient()
+// Use a workaround to solve the issue with prisma spawning too many clients in development
+// "warn(prisma-client) There are already 10 instances of Prisma Client actively running."
+// @see
+// https://www.prisma.io/docs/guides/database/troubleshooting-orm/help-articles/nextjs-prisma-client-dev-practices
+
+declare global {
+  var prisma: PrismaClient
+}
+
+let prisma: PrismaClient
+
+if (process.env.NODE_ENV === 'production') {
+  prisma = new PrismaClient()
+} else {
+  if (!global.prisma) {
+    global.prisma = new PrismaClient()
+  }
+  prisma = global.prisma
+}
+
+export { prisma }
